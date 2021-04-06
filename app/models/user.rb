8c8fc71bc4d26f32cdecfb2c7e94dd2c22281bd9
garciajordy <jordygarcia0119@gmail.com>
2021-04-06 12:24:47 +0300

    Adds error messages to form partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, length: {minimum: 3,maximum: 24}
+  validates :username, length: { minimum: 3, maximum: 24 }
   validates :email, presence: true
-  validates :password, length: {minimum: 6,maximum: 16}
+  validates :password, length: { minimum: 6, maximum: 16 }
 end
