f434395489140888c3bb34098367ff2eb1e93268
Zil Norvilis <zilasino27@gmail.com>
2021-04-06 14:06:47 +0200

    Add updates as requested per review

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user
+      redirect_to new_user_path
     else
       render :new
     end
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to @user
+      redirect_to edit_user_path(@user)
     else
       render :edit
     end
