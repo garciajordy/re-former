e75ccb0dab5c898aaa2dd87cd733093e9baef34f
Zil Norvilis <zilasino27@gmail.com>
2021-04-06 11:09:11 +0200

    Adds edit action to User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,25 @@ class UsersController < ApplicationController
         end
     end
 
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to @user
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to root_path
+    end
+
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
